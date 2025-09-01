67bccecadf1a4a1f42505558e66518b828184df5
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-09-01 15:22:27 +0500

    Test Greet component

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+	it("should render Hello with the name when name is provided", () => {
+		render(<Greet name="Hammad" />);
+		const heading = screen.getByRole("heading");
+		expect(heading).toBeInTheDocument();
+		expect(heading).toHaveTextContent(/Hammad/i);
+	});
+
+	it("should render Login button when name is not provided", () => {
+		render(<Greet name="" />);
+		const button = screen.getByRole("button");
+		expect(button).toBeInTheDocument();
+	});
+});
