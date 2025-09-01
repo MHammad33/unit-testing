c24965d81c662d1b910cb798b34ad9b929e38d83
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-09-01 15:24:38 +0500

    vitest global variables

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
 import Greet from "../../src/components/Greet";
 import "@testing-library/jest-dom/vitest";
 
